c7e413225331152ea97ebe28fbf399d8ebc41725
Remo Mattei <remo@mattei.org>
2021-11-18 21:03:01 -0800

    adding main.tf to test

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "itlinux/nginx-net-tools"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "prova"
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
+
